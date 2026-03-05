5333058ece3ce36fde38491ad6c7a03d062fe58a
Nagi <63901196+Nagiix@users.noreply.github.com>
2026-03-05 13:23:45 +0800

    hotfix slug comp

--- app/competition/[slug]/page.tsx
+++ app/competition/[slug]/page.tsx
@@ -226,9 +226,11 @@ export default function CompetitionDetailPage() {
                   </div>
                 </div>
               ) : (
-                <div className="text-[#F4D35E] font-bold text-[clamp(40px,4vw,50px)] drop-shadow-md">
-                  {data.price}
-                </div>
+                {typeof data.price === "string" && (
+                    <div className="text-[#F4D35E] font-bold text-[clamp(40px,4vw,50px)] drop-shadow-md">
+                      {data.price}
+                    </div>
+                  )}
               )}
             </div>
 
